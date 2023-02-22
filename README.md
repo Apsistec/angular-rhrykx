eca8d7e34d7b06267e4e08dac730cfcf0a82984a
Apsistec <doug@douglaswhite.app>
2023-02-22 13:52:24 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rhrykx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-df4uv9)
---
+++ src/app/extra-page/extra-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extra-page',
+  templateUrl: './extra-page.component.html',
+  styleUrls: ['./extra-page.component.css']
+})
+export class ExtraPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
